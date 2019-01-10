938727f9b532fd367fe12740675f3a152903ae0b
JiamingYang <1357723646@qq.com>
2019-01-10 09:53:01 +0800

    将测试代码的服务端和客户端分割开来

--- src/main/java/learn/rpc/client/RPCTest.java
+++ src/main/java/learn/rpc/client/RPCTest.java
@@ -1,14 +1,21 @@
 package learn.rpc.client;
 
+import learn.rpc.server.HelloService;
+import learn.rpc.server.HelloServiceImp;
+import learn.rpc.server.ServiceCenter;
+import learn.rpc.server.server;
+
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
+
 
 public class RPCTest {
     public static void main(String[] args) throws IOException {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        HelloService helloService = Client.getRemoteProxyObj(HelloService.class , new InetSocketAddress(8088));
+        Method[] methods = helloService.getClass().getMethods();
 
-            }
-        }).start();
+        Class<?> typeName = String.class;
+        System.out.print(helloService.sayHello("test"));
     }
 }
