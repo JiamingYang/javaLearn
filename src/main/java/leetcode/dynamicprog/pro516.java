604ac2599b6a75905ebcc39f698d3ed21e25fe18
JiamingYang <1357723646@qq.com>
2020-07-08 12:38:59 +0800

    leetcode516 动态规划

---
+++ src/main/java/leetcode/dynamicprog/pro516.java
@@ -0,0 +1,18 @@
+package leetcode.dynamicprog;/**
+ * Created by 13577 on 2020/7/7.
+ */
+
+/**
+ * @ClassName pro516
+ * @Description
+ * @Author 杨家铭
+ * @Date 2020/7/7 0:33
+ **/
+
+/**
+ * 最长回文子序列。如给定ccac  返回ccc
+ * 当首尾相等时dp[0][n-1]=dp[1][n-2]+2
+ * 否则 dp[0][n-1]=max(dp[0][n-1],dp[1][n-1])
+ */
+public class pro516 {
+}
