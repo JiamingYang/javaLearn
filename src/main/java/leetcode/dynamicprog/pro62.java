5e5f868777c975095a45dbf0c51ae1f7b219527f
JiamingYang <1357723646@qq.com>
2020-07-08 10:40:12 +0800

    leetcode62

---
+++ src/main/java/leetcode/dynamicprog/pro62.java
@@ -0,0 +1,17 @@
+package leetcode.dynamicprog;/**
+ * Created by 13577 on 2020/7/7.
+ */
+
+/**
+ * @ClassName pro62
+ * @Description
+ * @Author 杨家铭
+ * @Date 2020/7/7 0:29
+ **/
+
+/**
+ * 机器人在n*m的格子上，从左上角走到右下角，有多少种不同走法
+ * 递归公式;dp[i][j]=dp[i-1][j]+dp[i][j-1]
+ */
+public class pro62 {
+}
