8ea0ac339063dff70fa51886b4154c1f4be7e3f5
JiamingYang <1357723646@qq.com>
2020-07-08 10:43:55 +0800

    leetcode堆和前缀树

---
+++ src/main/java/leetcode/heap/pro347.java
@@ -0,0 +1,16 @@
+package leetcode.heap;/**
+ * Created by 13577 on 2020/6/1.
+ */
+
+/**
+ * @ClassName pro347
+ * @Description
+ * @Author 杨家铭
+ * @Date 2020/6/1 19:09
+ **/
+
+/**
+ * 给定一个非空整数数组，返回其中出现频率前k高的元素
+ */
+public class pro347 {
+}
---
+++ src/main/java/leetcode/pretree/pro212.java
@@ -0,0 +1,16 @@
+package leetcode.pretree;/**
+ * Created by 13577 on 2020/7/6.
+ */
+
+/**
+ * @ClassName pro212
+ * @Description
+ * @Author 杨家铭
+ * @Date 2020/7/6 18:38
+ **/
+
+/**
+ * 思路:使用深度优先算法
+ */
+public class pro212 {
+}
